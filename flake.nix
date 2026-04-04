f2d3f352e85b08a83aff21aaff8983602eadbffe
Lucas Mendelowski <lucas@mendelowski.com>
2026-04-04 22:18:10 +0200

    Switch to Nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.go_1_26
+            ];
+          };
+        });
+    };
+}
